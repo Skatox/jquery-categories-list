d1e322fb122f6e2d1d5d5e6f122cfb2358d14a59
Miguel Angel Useche Castro <migueluseche@skatox.com>
2023-06-02 18:23:56 -0500

    add translation files

--- jquery-categories-list.php
+++ jquery-categories-list.php
@@ -8,12 +8,25 @@
  * Requires PHP:      7.0
  * Author:            Miguel Angel Useche Castro
  * Author URI:        https://migueluseche.com/
- * License:           GPL2
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       jcl_i18n
  * Domain Path:       /languages
+ * License: GPL2
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Copyleft 2009-2023  Miguel Angel Useche Castro (email : migueluseche@skatox.com)
  *
- * @package          jquery-categories-list
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
